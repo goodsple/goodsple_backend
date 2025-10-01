7c6ee5b7b150d7b365075dbdb6d62e8d61b40893
soomin0927 <ehfvls0927@naver.com>
2025-10-01 20:57:55 +0900

    #86 검색 기능 - 키워드 검색 기능 구현

--- src/main/java/com/goodsple/features/search/service/SearchService.java
+++ src/main/java/com/goodsple/features/search/service/SearchService.java
@@ -1,4 +1,18 @@
 package com.goodsple.features.search.service;
 
+import com.goodsple.features.search.dto.SearchPostResponse;
+import com.goodsple.features.search.mapper.SearchMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
 public class SearchService {
+    private final SearchMapper searchMapper;
+
+    public List<SearchPostResponse> searchPosts(String keyword) {
+        return searchMapper.searchPosts(keyword);
+    }
 }
