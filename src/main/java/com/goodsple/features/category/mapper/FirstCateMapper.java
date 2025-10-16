746ad11716ede5ca1bb04d0e0dfb5fbd38f48713
BBB924 <qhrud4231@naver.com>
2025-10-16 22:03:23 +0900

    1차 카테고리는 메모만 수정가능하도록 코드 수정 #75

--- src/main/java/com/goodsple/features/category/mapper/FirstCateMapper.java
+++ src/main/java/com/goodsple/features/category/mapper/FirstCateMapper.java
@@ -14,7 +14,7 @@ public interface FirstCateMapper {
     @Select("SELECT * FROM first_cate WHERE first_cate_id = #{firstCateId}")
     FirstCate getFirstCateById(@Param("firstCateId") Long firstCateId);
 
-    @Insert("INSERT INTO first_cate (cate_name) VALUES (#{cateName}, #{subText})")
+    @Insert("INSERT INTO first_cate (cate_name, sub_text) VALUES (#{cateName}, #{subText})")
     @Options(useGeneratedKeys = true, keyProperty = "firstCateId")
     void insertCate(FirstCate firstCate);
 
